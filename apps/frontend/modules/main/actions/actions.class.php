d9b11cddff35b452a646330f0b8fff7ef346de65
hbt <hassenbentanfous@gmail.com>
2012-08-11 18:23:36 -0400

    adds one module

---
+++ apps/frontend/modules/main/actions/actions.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * main actions.
+ *
+ * @package    migration
+ * @subpackage main
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 2692 2006-11-15 21:03:55Z fabien $
+ */
+class mainActions extends sfActions
+{
+  /**
+   * Executes index action
+   *
+   */
+  public function executeIndex()
+  {
+    $this->forward('default', 'module');
+  }
+}
